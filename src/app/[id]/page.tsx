754c2c772bb9185c869af785e6af00e34c3253da
hcagatayyilmaz <62881149+hcagatayyilmaz@users.noreply.github.com>
2023-08-15 23:41:00 +0300

    learn isr & web vitals

---
+++ src/app/[id]/page.tsx
@@ -0,0 +1,31 @@
+import {FC} from 'react'
+
+interface pageProps {
+  params: {
+    id: string
+  }
+}
+
+export const revalidate = 600
+
+export async function generateStaticParams() {
+  const ids = ['1', '2', '3', '4']
+
+  return ids.map((id) => {
+    return {
+      id
+    }
+  })
+}
+
+const page: FC<pageProps> = ({params}) => {
+  console.log(params)
+  return (
+    <div>
+      page
+      <h1>bum</h1>
+    </div>
+  )
+}
+
+export default page
